554cc91098cf0386d9abc5cb352ef56ed0d3df85
20essentials <s0l0estudi4r@gmail.com>
2024-05-22 00:59:04 +0200

    v0.0.2

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 console.group("REFERENCE");
 console.log("PYRAMID", "https://uiverse.io/alexmaracinaru/heavy-baboon-91");
+console.log("img-1 ->", "https://wallhaven.cc/w/2k2gy9");
+console.log("img-2 ->", "https://wallhaven.cc/w/396lmd");
+console.log("img-3 ->", "https://wallhaven.cc/w/9mgjq1");
+console.log("img-4 ->", "https://wallhaven.cc/w/0wv5xx");
+console.log("img-5 ->", "https://wallhaven.cc/w/nzm8ky");
+console.log("img-6 ->", "https://wallhaven.cc/w/ne83mo");
 console.groupEnd();
